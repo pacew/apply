80b146965e54500959fadbbece5326bb32aa1389
Pace Willisson <pace@alum.mit.edu>
2026-04-21 18:41:23 -0400

    sound

--- sound_permissions.php
+++ sound_permissions.php
@@ -17,7 +17,7 @@ read_notify_info();
 $evts = [];
 
 foreach ($webgrid as $webgrid_elt) {
-    if (strcmp ($webgrid_elt->room, "Sterling") != 0)
+    if (strcmp ($webgrid_elt->room, "Seminar") != 0)
         continue;
 
     $evids = "";
@@ -29,13 +29,32 @@ foreach ($webgrid as $webgrid_elt) {
         if (($conf = @$confirmations[$name_id]) != NULL)
             $record = intval($conf->record);
 
+        $group_name = "";
+        $leader_id = 0;
+        $name_id = 0;
         foreach ($webgrid_elt->evids as $evid) {
             if (($app = @$apps_by_evid[$evid]) != NULL) {
                 $t = sprintf ("index.php?app_id=%d", $app->app_id);
                 $evids .= mklink ($evid, $t);
                 $evids .= " ";
+
+                $name_id = $app->neffa_id;
+                if (strcmp ($app->curvals['main_performer'], "Group") == 0) {
+                    $group_name = $app->curvals['group_name'];
+                    $group_id = name_to_id ($group_name);
+                    $leader_id = @$group_to_group_leader[$group_id];
+
+                }
             }
         }
+    }
+
+    if ($leader_id)
+        $name_id = $leader_id;
+
+    $contact_name = "";
+    if (($perf = @$performers[$name_id]) != NULL) {
+        $contact_name = $perf->name;
     }
 
     $evt = (object)NULL;
@@ -43,6 +62,8 @@ foreach ($webgrid as $webgrid_elt) {
     $evt->eventid = $webgrid_elt->eventid;
     $evt->evids = $evids;
     $evt->record = $record;
+    $evt->group_name = $group_name;
+    $evt->contact_name = $contact_name;
     $evts[] = $evt;
 
 }
@@ -59,11 +80,18 @@ foreach ($evts as $evt) {
     $cols[] = h($evt->eventid);
     $cols[] = h($evt->record);
     $cols[] = h($evt->title);
+    $cols[] = h($evt->contact_name);
+    $cols[] = h($evt->group_name);
     $cols[] = $evt->evids;
     $rows[] = $cols;
 }
 
 $body .= "<p>record response 3 means 'No'</p>";
-$body .= mktable (array("eventid", "record response", "title", "evids"), $rows);
+$body .= mktable (array("eventid", 
+        "record response", 
+        "title", 
+        "contact",
+        "group",
+        "evids"), $rows);
 
 pfinish();
